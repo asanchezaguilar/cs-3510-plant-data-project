819d598a65dd88633ff10e1222b7de71354429eb
asanchezaguilar <asanchezaguilar@csustan.edu>
2025-11-03 19:43:23 +0000

    imported data file, starting working on system

---
+++ system.py
@@ -0,0 +1,17 @@
+from plant.py import Plant
+import csv
+
+class PlantIRS():
+    def __init__(self):
+        self._plant_list = []
+
+    def load_data(self):
+        pass
+    
+    def add_plant(self, plant):
+        self._plant_list.append(plant)
+
+    def average(self):
+        pass
+
+    # def graphs...
