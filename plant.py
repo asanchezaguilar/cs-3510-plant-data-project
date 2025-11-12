3b6dad1ff753bd8c6782a07861f84dd3fec9b7b9
asanchezaguilar <asanchezaguilar@csustan.edu>
2025-11-12 17:23:29 +0000

    added all setters, tested print all plants

--- plant.py
+++ plant.py
@@ -1,5 +1,5 @@
 class Plant:
-    def __init__(self, timestamp="none", plant_id="none", soil_moisture=0, ambient_temperature=0, soil_temperature=0, humidity=0, light_intensity=0, soil_ph=0, nitrogen_level=0, phosphorus_level=0, potassium_level=0, chlorophyll_content=0, electrochemical_signal=0, plant_health_status="none"):
+    def __init__(self, timestamp="none", plant_id="none", soil_moisture=0, ambient_temperature=0, soil_temperature=0, humidity=0, light_intensity=0, soil_ph=0, nitrogen_level=0, phosphorus_level=0, potassium_level=0, chlorophyll_content=0, electrochemical_signal=0, plant_health_status="none", unique_id = 0):
         self._timestamp = timestamp
         self._plant_id = plant_id
         self._soil_moisture = soil_moisture
@@ -14,6 +14,7 @@ class Plant:
         self._chlorophyll_content = chlorophyll_content
         self._electrochemical_signal = electrochemical_signal
         self._plant_health_status = plant_health_status
+        self._unique_id = unique_id
 
     # getters
     @property
@@ -30,7 +31,7 @@ class Plant:
         return self._plant_id
 
     @plant_id.setter
-    def plant_id(self, plant_id)
+    def plant_id(self, plant_id):
         if plant_id:
             self._plant_id = plant_id
 
@@ -38,49 +39,109 @@ class Plant:
     def soil_moisture(self):
         return self._soil_moisture
 
+    @soil_moisture.setter
+    def soil_moisture(self, soil_moisture):
+        if soil_moisture:
+            self._soil_moisture = soil_moisture
+
     @property
     def ambient_temperature(self):
          return self._ambient_temperature
 
+    @ambient_temperature.setter
+    def ambient_temperature(self, ambient_temperature):
+        if ambient_temperature:
+            self._ambient_temperature = ambient_temperature
+
     @property
     def soil_temperature(self):
         return self._soil_temperature
+    
+    @soil_temperature.setter
+    def soil_temperature(self, soil_temperature):
+        if soil_temperature:
+            self._soil_temperature = soil_temperature
 
     @property
     def humidity(self):
         return self._humidity
 
+    @humidity.setter
+    def humidity(self, humidity):
+        if humidity:
+            self._humidity = humidity
+
     @property
     def light_intensity(self):
         return self._light_intensity
+
+    @light_intensity.setter
+    def light_intensity(self):
+        if light_intensity:
+            self._light_intensity = light_intensity
   
     @property
     def soil_ph(self):
         return self._soil_ph
 
+    @soil_ph.setter
+    def soil_ph(self, soil_ph):
+        if soil_ph:
+            self._soil_ph = soil_ph
+
     @property
     def nitrogen_level(self):
         return self._nitrogen_level
+
+    @nitrogen_level.setter
+    def nitrogen_level(self, nitrogen_level):
+        if nitrogen_level:
+            self._nitrogen_level = nitrogen_level
 
     @property
     def phosphorus_level(self):
         return self._phosphorus_level
 
+    @phosphorus_level.setter
+    def phosphorus_level(self, phosphorus_level):
+        if phosphorus_level:
+            self._phosphorus_level = phosphorus_level
+
     @property
     def potassium_level(self):
         return self._potassium_level
+
+    @potassium_level.setter
+    def potassium_level(self, potassium_level):
+        if potassium_level:
+            self._potassium_level = potassium_level
         
     @property
     def chlorophyll_content(self):
         return self._chlorophyll_content
     
+    @chlorophyll_content.setter
+    def chlorophyll_content(self, chlorophyll_content):
+        if chlorophyll_content:
+            self._chlorophyll_content = chlorophyll_content
+    
     @property
     def electrochemical_signal(self):
         return self._electrochemical_signal
+
+    @electrochemical_signal.setter
+    def electrochemical_signal(self, electrochemical_signal):
+        if electrochemical_signal:
+            self._electrochemical_signal = electrochemical_signal
     
     @property
     def plant_health_status(self):
         return self._plant_health_status
+
+    @plant_health_status.setter
+    def plant_health_status(self, plant_health_status):
+        if plant_health_status:
+            self._plant_health_status = plant_health_status
 
     def __str__(self):
         return f"Plant info: Timestamp: {self.timestamp}, Plant ID: {self.plant_id}, Soil Moisture: {self.soil_moisture}%, Ambient Temperature: {self.ambient_temperature}°C, Soil Temperature: {self.soil_temperature}°C, Humidity: {self.humidity}% Light Intensity: {self.light_intensity}, Soil pH: {self.soil_ph}, Nitrogen Level: {self.nitrogen_level}%, Phosphorus Level: {self.phosphorus_level}%, Potassium Level: {self.potassium_level}%, Chlorophyll Content: {self.chlorophyll_content}, Electrochemical Signal: {self.electrochemical_signal}, Plant Health Status: {self.plant_health_status}"
--- system.py
+++ system.py
@@ -7,27 +7,29 @@ class PlantIRS():
     def load_data(self):
         with open("plant_health_data.csv") as csv_file:
             reader = csv.reader(csv_file)
-
+            unique_id = 1
             for row in reader:
                 if row[0] == "Timestamp":
                     pass
                 else:
                     plant = Plant()
-                    plant.timestamp = row[0]
-                    plant.plant_id = row[1]
-                    plant.soil_moisture = row[2]
-                    plant.ambient_temperature = row[3]
-                    plant.soil_temperature = row[4]
-                    plant.humidity = row[5]
-                    plant.light_intensity = row[6]
-                    plant.soil_ph = row[7]
-                    plant.nitrogen_level = row[8]
-                    plant.phosphorus_level = row[9]
-                    plant.potassium_level = row[10]
-                    plant.chlorophyll_content = row[11]
-                    plant.electrochemical_signal = row[12]
-                    plant.plant_health_status = row[13]
+                    timestamp = row[0]
+                    plant_id = row[1]
+                    soil_moisture = row[2]
+                    ambient_temperature = row[3]
+                    soil_temperature = row[4]
+                    humidity = row[5]
+                    light_intensity = row[6]
+                    soil_ph = row[7]
+                    nitrogen_level = row[8]
+                    phosphorus_level = row[9]
+                    potassium_level = row[10]
+                    chlorophyll_content = row[11]
+                    electrochemical_signal = row[12]
+                    plant_health_status = row[13]
+                    plant = Plant(timestamp, plant_id, soil_moisture, ambient_temperature, soil_temperature, humidity, light_intensity, soil_ph, nitrogen_level, phosphorus_level, potassium_level, chlorophyll_content, electrochemical_signal, plant_health_status, unique_id)
                     self._plant_list.append(plant)
+                    unique_id += 1
     
     def add_plant(self, plant):
         self._plant_list.append(plant)
